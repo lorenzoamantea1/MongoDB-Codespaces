d80424d73ff77c7b73e4562d0d35dc754addb735
Lorenzo Amantea <lorenzo.amantea1@gmail.com>
2025-11-14 11:23:01 +0000

    Aggiorna le dimensioni dell'immagine nel README.md e migliora i messaggi di log in index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose"
 import express from "express"
 
 const app = express();
+const PORT = 3000;
 
 mongoose.connect('mongodb://localhost:27017/mydb')
-  .then(() => console.log('MongoDB connesso'))
+  .then(() => console.log('✅ Connesso a MongoDB'))
   .catch(err => console.error('Errore MongoDB:', err));
 
 app.get('/', (req, res) => {
   res.send("MongoDB & Express on Codespace!");
 });
 
-app.listen(3000, () => {console.log("Server in ascolto su porta 3000");});
+app.listen(PORT, () => {console.log(`Server in ascolto su porta :${PORT}`);});
